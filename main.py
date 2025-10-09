da1e7b9aae90da0cfafd77c2bc0b9ddf135a4122
Jakub Slatinsky <slatinskyj@gmail.com>
2025-10-09 15:54:57 +0200

    Update main.py to accept user prompts as input for content generation

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import sys
 import os
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    prompt = " ".join(args)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
