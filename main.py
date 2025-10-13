9d130d0f06f18d29b82c870fec7492c140b0b33c
Jakub Slatinsky <slatinskyj@gmail.com>
2025-10-13 17:51:09 +0200

    Refactor content generation to use structured messages instead of raw prompts
    Signed-off-by: Jakub Slatinsky <slatinskyj@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -14,13 +15,16 @@ def main():
         sys.exit(1)
     prompt = " ".join(args)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
